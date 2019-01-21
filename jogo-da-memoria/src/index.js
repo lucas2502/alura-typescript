8af22de851f1c3a01707da780a87368cbbe99289
lucas2502 <lucas2502@live.com>
2019-01-21 23:10:59 +0000

    Criando componentes com JavaScript

--- jogo-da-memoria/src/index.js
+++ jogo-da-memoria/src/index.js
@@ -1,23 +1,14 @@
 const $root = document.querySelector("#root");
-const $memoryCard = document.createElement("article");
 const $memoryCardFront = document.createElement("article");
+createMemoryCard();
 
-const $icon = `<img 
-    src='img/icon-collabcode.png' 
-    alt='Gaio Mascote da Collabcode' 
-    class='icon'
-    >`
 const $iconC = `<img 
     src='img/icon-c.png' 
     alt='Book C++' 
     class='icon'>`
 
-$memoryCard.classList.add("memory-card");
-$root.insertBefore($memoryCard, null);
-
 $memoryCardFront.classList.add("memory-card");
 $memoryCardFront.classList.add("-front");
-$root.insertBefore($memoryCardFront, $memoryCard);
+$root.insertBefore($memoryCardFront, null);
 
-$memoryCard.insertAdjacentHTML("afterbegin", $icon);
 $memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
