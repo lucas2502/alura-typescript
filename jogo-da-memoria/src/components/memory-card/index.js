9c7dd540b7aeffdeef3a41290ed125fdf27cf1fc
lucas2502 <lucas2502@live.com>
2019-01-26 22:00:47 +0000

    Challenge 26 insert css by JavaScrip

--- jogo-da-memoria/src/components/memory-card/index.js
+++ jogo-da-memoria/src/components/memory-card/index.js
@@ -8,6 +8,57 @@ const createMemoryCard = (card) => {
     `;
     
 };
+
+(function style(){
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+    
+
+    $style.textContent = `
+        :root{
+            --warm-pink: #f25a70;
+        }
+        
+        .memory-card{
+            width: 155px;
+            height: 155px;
+            box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+            background-color: var(--warm-pink);
+            border-radius: 30px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            cursor: pointer;
+        }
+        
+        .memory-card.-front{
+            background-color: #fff;
+        }
+        
+        .memory-card.-front::before{
+            content:"";
+            width: 100px;
+            height: 100px;
+            background-color: #d4d4d4;
+            border-radius: 50%;
+            position: absolute;
+        }
+        
+        .memory-card > .icon {
+            width: 100px;
+            height: 100px;
+        }
+        
+        .memory-card.-front > .icon {
+            position: absolute;
+            transform: translateY(-10px);
+        }   
+    `;
+
+    $head.insertBefore($style, null);
+})();
+
 /*
 function createMemoryCardFront(){
     const $memoryCardFront = `
--- jogo-da-memoria/src/objects/cards-wrapper/index.js
+++ jogo-da-memoria/src/objects/cards-wrapper/index.js
@@ -1,5 +1,22 @@
 function createCardsWrapper(){
     const $cardsWrapper = document.createElement("section");
     $cardsWrapper.classList.add("cards-wrapper");
+    
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent =  `
+        .cards-wrapper{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+        }
+        .cards-wrapper > .memory-card{
+            margin: 10px 0px 10px 0px;
+        }
+    `;
+    
+    $head.insertBefore($style, null);
+    
     return $cardsWrapper;
 }
