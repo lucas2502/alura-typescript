67b5bc1c6e211f1ddb338b52f71bee8530c157cb
lucas2502 <lucas2502@live.com>
2019-06-30 21:40:46 -0300

    Create Settings folder size file ITCSS

--- jogo-da-velha/jogo-da-velha/src/styles/base/index.js
+++ jogo-da-velha/jogo-da-velha/src/styles/base/index.js
@@ -2,10 +2,16 @@ import { createGlobalStyle } from 'styled-components'
 
 export const Base = createGlobalStyle `
     html, body {
-
+        background-color: var(--color-base);
+        font-size: var(--size-base);
+        color: var(--color-first);
+        min-width: 300px;
     }
 
     body {
-
+        height: 100%;
+        max-width: 100%;
+        position: relative;
+        
     }
 `
