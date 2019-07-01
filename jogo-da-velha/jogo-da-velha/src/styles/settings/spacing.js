7e609c4c7be846b726c4bb0737d733d596bb3ffa
lucas2502 <lucas2502@live.com>
2019-06-30 21:41:28 -0300

    Create Settings folder spacing file ITCSS

---
+++ jogo-da-velha/jogo-da-velha/src/styles/settings/spacing.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const Spacing = createGlobalStyle`
+    :root {
+        --spacing-small: 1rem;
+        --spacing-medium: 2rem;
+        --spacing-big: 4rem;
+        --spacing-very-big: 5rem;
+        --spacing-over-big: 120px;
+        --spacing-menu: 80px;
+        --spacing-active-menu: 340px;
+        --spacing-page-menu: 5vw;
+    }
+    @media (max-height: 700px) {
+        :root {
+            --spacing-small: 0.5rem;
+            --spacing-big: 2rem;
+        }
+    }
+`
