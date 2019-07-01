9a6a78ff7adb3f20022fb4987d9afa56eee68f5c
lucas2502 <lucas2502@live.com>
2019-07-01 00:14:12 -0300

    Create object Logo/Menu and crate component Header

---
+++ jogo-da-velha/jogo-da-velha/src/objects/MenuBurguer/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css'
+
+
+const MenuBurguer = () => {
+    return (
+        <>
+            <span className="span"/>
+            <span className="span"/>
+            <span className="span"/>
+        </>
+    )
+}
+
+export default MenuBurguer;
