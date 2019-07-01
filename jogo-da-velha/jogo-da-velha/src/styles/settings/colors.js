ce1f066bb3f5ab569c3800f893f024f041e02da8
lucas2502 <lucas2502@live.com>
2019-06-30 21:29:37 -0300

    Create Settings folder ITCSS

---
+++ jogo-da-velha/jogo-da-velha/src/styles/settings/colors.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const Colors = createGlobalStyle `
+    :root {
+        --color-negative-light: rgba( 0, 0, 0, 0.15);
+        --color-negative: #000;
+        --color-base-light: ;
+        --color-base: #eeeeee;
+        --color-zero: #fff;
+        --color-first: #707070;
+        --color-second: #3a4042;
+    }
+`
