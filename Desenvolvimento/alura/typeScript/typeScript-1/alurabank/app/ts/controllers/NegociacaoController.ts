e6bee4cbfe8541c21aaa7cf37ce5b48aea4ce567
lucas2502 <lucas2502@live.com>
2019-08-06 11:39:15 -0300

    Template dinâmico

--- Desenvolvimento/alura/typeScript/typeScript-1/alurabank/app/ts/controllers/NegociacaoController.ts
+++ Desenvolvimento/alura/typeScript/typeScript-1/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -4,13 +4,14 @@ class negociacaoController {
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
-    private _negociacoesView = new NegociacoesView('#negociacoesViews');
+    private _negociacoesView = new NegociacoesView("#negociacoesView");
 
     constructor() {
 
         this._inputData = <HTMLInputElement>document.querySelector("#data");
         this._inputQuantidade = <HTMLInputElement>document.querySelector("#quantidade");
         this._inputValor = <HTMLInputElement>document.querySelector("#valor");
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event){
@@ -25,11 +26,7 @@ class negociacaoController {
 
         this._negociacoes.adiciona(negociacao);
 
-        this._negociacoes.paraArray().forEach(negociacao => {
-            console.log(negociacao.data);
-            console.log(negociacao.quantidade);
-            console.log(negociacao.valor);
-        });
+        this._negociacoesView.update(this._negociacoes);
     }
 
 }
